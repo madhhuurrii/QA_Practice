25878ee4acdca110c36b8d2c02e3e8e491924029
Madhuri Ramakrishnan <64744084+madhhuurrii@users.noreply.github.com>
2025-05-24 10:12:43 +0000

    static table

--- src/main/java/setup/WebTables.java
+++ src/main/java/setup/WebTables.java
@@ -3,8 +3,10 @@ package setup;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.openqa.selenium.*;
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
+
+import java.time.Duration;
 
 public class WebTables {
   static protected WebDriver driver;
@@ -16,8 +18,20 @@ public class WebTables {
   }
 
   public static void main(String[] args) {
-    driver.get("https://github.com/madhhuurrii/");
+    driver.get("https://testautomationpractice.blogspot.com/");
     driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    // no. of rows
+    int r = driver.findElements(By.xpath("//table[@name='BookTable']//tr")).size();
+    int c = driver.findElements(By.xpath("//table[@name='BookTable']//tr[1]//th")).size();
+    System.out.print(c);
+    // starts with 1 not 0
+    for(int i=2;i<r;i++){
+      String a = driver.findElement(By.xpath("//table[@name='BookTable']//tr["+i+"]//td[1]")).getText();
+      System.out.println(a);
+
+    }
+    
     // driver.quit();
   }
 }

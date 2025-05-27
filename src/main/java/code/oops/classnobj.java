d616b426ea17bed48e258b05db20018b617a3d02
Madhuri Ramakrishnan <64744084+madhhuurrii@users.noreply.github.com>
2025-05-27 15:27:03 +0000

    add

--- src/main/java/code/oops/classnobj.java
+++ src/main/java/code/oops/classnobj.java
@@ -1,6 +1,17 @@
 package code.oops;
 
 public class classnobj {
+
+  int x;
+  String m;
+  void display(){
+    System.out.println(x+" "+m);
+  }
+
+  classnobj(int a){
+      x=a;
+      System.out.println(x);
+  }
   public static void main(String[] args) {
     // class is collection of attributes and behaviour
     // without class there is no object
@@ -22,6 +33,20 @@ public class classnobj {
     // object is physical entity/ occupies space in memory
     // Employee emp = new Employee();
 
+    // Constructor
+    // Constructor takes param but has no return type
+    // Constructor is same name as classname
+    // it is initiated as soon as the object is declared
+    // 2 types: default and parameterized constructor
+    // only used for assigning value no logics
+
+    
+     classnobj c = new classnobj(5); // object declaration of class
+     c.x=10;
+     c.m="Madhuri";
+     c.display();
+    //  classnobj c1 = new classnobj(11);
+    //  c1.display();
 
 
 
---
+++ src/main/java/code/oops/polymorphism.java
@@ -0,0 +1,16 @@
+package code.oops;
+
+public class polymorphism {
+  public static void main(String[] args) {
+    // one thing in many forms
+    // we can achieve polymorphism using overloading
+    // method overloading, constructor overloading
+
+    // method overloading:
+    // 1. method name should be same
+    // 2. no. of parameter should be different
+    // 3. data type of paramter should be different
+    // 4. order of paramter should be different
+
+  }
+}

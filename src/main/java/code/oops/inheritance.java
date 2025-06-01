b62ca1e493266d3a28f45275a1b130c3bbe43406
Madhuri Ramakrishnan <64744084+madhhuurrii@users.noreply.github.com>
2025-06-01 17:26:02 +0000

    inheritance

--- src/main/java/code/oops/inheritance.java
+++ src/main/java/code/oops/inheritance.java
@@ -1,20 +1,20 @@
-package code.oops;
+// package code.oops;
 
-// Multiple uses Interface concept
-interface A{
-  String s();
-}
-interface B{
-  String s1();
-}
-class c implements A, B{
-  String c9;
-}
-public class inheritance {
-  ///
-  /// Inheritance: acquiring all the properties& behaviour from one class to another class
-  /// obj: reusability , avoid duplication
-  /// 1. single 2. multiple 3. hierarchy 4. multilevel
-  /// 
-  ///
-}
+// // Multiple uses Interface concept
+// interface A{
+//   String s();
+// }
+// interface B{
+//   String s1();
+// }
+// class c implements A, B{
+//   String c9;
+// }
+// public class inheritance {
+//   ///
+//   /// Inheritance: acquiring all the properties& behaviour from one class to another class
+//   /// obj: reusability , avoid duplication
+//   /// 1. single 2. multiple 3. hierarchy 4. multilevel
+//   /// 
+//   ///
+// }

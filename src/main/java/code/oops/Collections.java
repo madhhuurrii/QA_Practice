65a539ca958851ddc882619bb44986e81d9fd70a
Madhuri Ramakrishnan <64744084+madhhuurrii@users.noreply.github.com>
2025-06-02 12:37:12 +0000

    Exception Handling

---
+++ src/main/java/code/oops/Collections.java
@@ -0,0 +1,7 @@
+package code.oops;
+
+public class Collections {
+  public static void main(String[] args) {
+    
+  }
+}
--- src/main/java/code/oops/exceptionHandling.java
+++ src/main/java/code/oops/exceptionHandling.java
@@ -4,8 +4,18 @@ public class exceptionHandling {
   // Exception is an event which cause sudden termination of program (user mistake)
   
   // Errors - syntax, logical (caused by programmers)
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws ArithmeticException {
+    try{
+      System.out.println(100/0);
+
+    }
+    catch(Exception e){
+      System.out.println(e.getMessage());
+    } // 1 try can have multiple catch blocks
+    finally{
+      // block of code which needs to be executed anyhow
+      System.out.println("Finally executed!");
+    }
   }
 }
 
@@ -14,3 +24,12 @@ public class exceptionHandling {
 ///    [ InteruptedException, FileNotFound, IOException etc.]
 /// 2) Unchecked Exceptions
 ///    [ArithmeticException, NullPointerException, ArrayIndexOutofBound etc.]
+/// 
+/// ArithmeticException - In which a number is divided by 0
+/// NullPointerException - Performing operations on null variables
+/// ArrayIndexOutofBound - trying to access value out of size of array
+/// NumberFormatException - if string('welcome', instead of '1234') is passed to parse it to Int
+/// 
+/// 
+/// 
+/// where inputs is taken, high chance of exception

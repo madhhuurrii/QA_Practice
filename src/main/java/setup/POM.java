0ebce1d96833f9972a0ddf21b816ae680310067b
Madhuri Ramakrishnan <64744084+madhhuurrii@users.noreply.github.com>
2025-06-05 16:31:58 +0000

    POM script

---
+++ src/main/java/setup/POM.java
@@ -0,0 +1,16 @@
+package setup;
+
+public class POM{
+   public static void main(String[]args){
+      
+   }
+}
+
+
+// POM
+// It is a design pattern
+// / Dealing with Pages as seperate Java Classes
+// / Script is easier to maintain and understand
+// / Every page in the appliaction will have a seperate JavaClass
+// / All WebElements will be declared as variables
+// / Corresponding methods acting on variables
---
+++ src/main/java/setup/POM.md
@@ -0,0 +1,10 @@
+POM
+It is a design pattern
+Dealing with Pages as seperate Java Classes
+Script is easier to maintain and understand
+Every page in the appliaction will have a seperate JavaClass
+All WebElements will be declared as variables
+Corresponding methods acting on variables
+
+
+![alt text](image.png)
